c91b5f3056c4fdcd950cbf6f2569a4c48d6e495c
Andrii <578848@student.saxion.nl>
2026-01-11 02:38:16 -0800

    Delete src/main directory

--- src/main/java/nl/saxion/game/dungeonheart/componenets/Texture.java
+++
@@ -1,8 +0,0 @@
-package nl.saxion.game.dungeonheart.componenets;
-
-public class Texture extends Component {
-    public Texture(int width, int height, String textureId) {
-        super(width, height, textureId, textureId);
-        this.componentsMap.put("textures", this);
-    }
-}
